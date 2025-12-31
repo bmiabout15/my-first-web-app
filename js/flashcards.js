37c51fedc5f92958cbb4132a30465830d03ad46e
bmiabout15 <kouta14rpm@gmail.com>
2025-12-31 15:43:05 +0900

    単語帳の追加

--- js/flashcards.js
+++ js/flashcards.js
@@ -1,73 +1,124 @@
-const flashcardsList = document.getElementById("flashcards-list");
-
-// サーバーからデータを取得する関数を作成してください
-export async function fetchFlashcards(){
-  try{
+// get flashcards list
+async function fetchFlashcards() {
+  try {
     const response = await fetch("/api/flashcards");
-    return await response.json();
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-  catch{
-    console.log("エラーが発生しました");
+}
+
+// 暗記カードを追加するデータをサーバーに送る関数を作成してください
+async function createFlashcardData(wordData) {
+  try{
+    const url = "/api/flashcards";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(wordData),
+    });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
     return [];
   }
 }
 
 export async function setupFlashcards() {
-  // 暗記カード機能に必要な処理を作成してください
-  async function renderFlashcards(wordList){
-    flashcardsList.innerHTML='';
-    for (const word of wordList) {
-      const flashcard = `<div class="flashcard">
+  const flashcardsList = document.getElementById("flashcards-list");
+  function toggleMeaning(id) {
+    const meaningElement = document.querySelector(`[data-meaning="${id}"]`);
+
+    if (meaningElement.classList.contains("hidden")) {
+      meaningElement.classList.remove("hidden");
+    } else {
+      meaningElement.classList.add("hidden");
+    }
+  }
+
+  async function readFlashcards() {
+    const wordList = await fetchFlashcards();
+    renderFlashcards(wordList);
+  }
+
+  async function renderFlashcards(wordList) {
+    flashcardsList.innerHTML = "";
+    wordList.forEach((word) => {
+      const flashcard = `
+      <div class="flashcard">
         <div class="flashcard-content">
           <p class="flashcard-title">${word.word}</p>
           <div class="flashcard-icons">
-            <button class="flashcard-meaning" data-toggle="${word.id}">
+            <button data-toggle="${word.id}" class="flashcard-meaning">
               <span class="ri-eye-line"></span>
             </button>
           </div>
         </div>
         <div data-meaning="${word.id}" class="hidden">
-          <p>${word.meaning}</p>
+          <p class="flashcard-toggle">${word.meaning}</p>
         </div>
-      </div>`;
-
+      </div>
+      `;
       flashcardsList.innerHTML += flashcard;
-    }
+    });
   }
 
-  async function readFlashcards(){
-    const wordList = await fetchFlashcards();
-    renderFlashcards(wordList);
-  }
+  flashcardsList.addEventListener("click", event => {
+    const btn = event.target.closest(".flashcard-meaning");
+    if(btn){
+      const id = btn.getAttribute("data-toggle");
+      toggleMeaning(id);
+    }else{
+      return;
+    }
+  });
 
   await readFlashcards();
 
-  function toggleMeaning(id){
-    const cardMeaningElement = `[data-meaning="${id}"]`;
-    const meaningElement = document.querySelector(cardMeaningElement);
-    if (meaningElement) {
-      const hasClass = meaningElement.classList.contains("hidden");
-      
-      if(hasClass)meaningElement.classList.remove("hidden");
-      else meaningElement.classList.add("hidden");
-    }
-    else {
-      console.log('要素が見つかりませんでした。');
-    }
+
+  //モーダル
+  const addWord = document.querySelector(".add-word");//ボタン
+  const wordModal = document.getElementById("word-modal");//モーダル
+  const wordForm = document.getElementById("word-form");//モーダル内フォーム
+  const cancelWord = document.querySelector(".cancel-word");//ボタン
+
+  function showModal(){
+    wordModal.classList.remove("hidden");
+    const wordInput = document.getElementById("word-input");//自動カーソル
+    wordInput.focus();
   }
 
-  flashcardsList.addEventListener("click", (event) =>{
-    // クリックされた要素から最も近い .flashcard-meaning 要素を探す
-    const clickedMeaningElement = event.target.closest('.flashcard-meaning');
+  function hideModal(){
+    wordModal.classList.add("hidden");
+    wordForm.reset();
+  }
 
-    if (clickedMeaningElement) {
-        const cardId = clickedMeaningElement.dataset.toggle;
-        console.log('暗記カードのID:', cardId);
-        toggleMeaning(cardId);
+  async function save(event) {
+    event.preventDefault();
+
+    const wordInput = document.getElementById("word-input").value;
+    const meaningInput = document.getElementById("meaning-input").value;
+
+    const word = {
+      id: Date.now(),
+      word: wordInput.trim(),
+      meaning: meaningInput.trim(),
+    };
+
+    await createFlashcardData(word);
+    await readFlashcards();
+    hideModal();
+  }
+
+  addWord.addEventListener("click", showModal);
+  cancelWord.addEventListener("click", hideModal);
+  wordModal.addEventListener("click", event=>{
+    if (event.target === wordModal) {
+        hideModal();
     }
-    else{
-      return;
-    }
-    return;
   });
+
+  const saveWord = document.querySelector(".save-word");//ボタン
+  saveWord.addEventListener("click", save);
 }
